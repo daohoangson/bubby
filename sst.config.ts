7a6e1244fb9e7bba18b264fe8051e8c87b7d9db4
Dao Hoang Son <daohoangson@gmail.com>
2023-11-12 14:16:36 +0700

    Deploy prod via GitHub Actions

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import { SSTConfig } from "sst";
 import { API } from "./stacks/MyStack";
+import { GitHub } from "./stacks/ProdStack";
 
 export default {
   config(_input) {
@@ -9,6 +10,6 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(API);
-  }
+    app.stack(API).stack(GitHub);
+  },
 } satisfies SSTConfig;
