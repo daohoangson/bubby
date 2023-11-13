89b43d07950f5f0a3bec908622c4bc053440c16a
Dao Hoang Son <daohoangson@gmail.com>
2023-11-13 17:48:12 +0700

    Simplify `reply_to_chat.ts`

--- packages/core/src/handlers/reply_to_chat.ts
+++ packages/core/src/handlers/reply_to_chat.ts
@@ -12,28 +12,23 @@ export async function* replyToPhoto(
     chat: ChatPhoto;
   }
 ): AsyncGenerator<Reply> {
-  const threadId = await assistantThreadIdUpsert(input);
   const caption = input.chat.getPhotoCaption() ?? "";
   const photoUrl = input.chat.getPhotoUrl();
   const message = `${caption}\n\n${photoUrl}`;
-  yield* repliesGenerator(input, threadId, message);
+  yield* repliesGenerator(input, message);
 }
 
-export async function* replyToText(
+export const replyToText = (
   input: AssistantThreadInput & {
     chat: ChatText;
   }
-): AsyncGenerator<Reply> {
-  const threadId = await assistantThreadIdUpsert(input);
-  yield* repliesGenerator(input, threadId, input.chat.getTextMessage());
-}
+) => repliesGenerator(input, input.chat.getTextMessage());
 
 async function* repliesGenerator(
   input: AssistantThreadInput,
-  threadId: string,
   message: string
 ): AsyncGenerator<Reply> {
-  console.log({ threadId, message });
+  const threadId = await assistantThreadIdUpsert(input);
   const { runId } = await assistantSendMessage(threadId, message);
   yield* assistantWaitForRun({ ...input, threadId, runId });
   yield* assistantGetNewMessages(threadId, runId);
